6fa4e0e6021b623e9adaf8d61a4123d15a9658ba
DevJMoab <devjmoab@hotmail.com>
2020-07-06 22:36:11 -0300

    solve challenge part1

---
+++ challenge4/challenge4.js
@@ -0,0 +1,24 @@
+const user = {
+    nome: "JMoab",
+    transactions: [],
+    balance: 0
+}
+
+function createTransaction(transaction, value) {
+
+    if (transaction === "credit") {
+        user.balance = user.balance + value
+        user.transactions.push(value)
+    } else {
+        user.balance = user.balance - value
+        user.transactions.push("-" + value)
+    }
+    console.log(user);
+
+}
+
+createTransaction("credit", 10)
+createTransaction("credit", 10)
+createTransaction("credit", 10)
+createTransaction("debit", 10)
+createTransaction("debit", 10)
