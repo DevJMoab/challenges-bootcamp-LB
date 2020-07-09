dc8c10320a97516209d54140d09e9f0e45433d64
DevJMoab <devjmoab@hotmail.com>
2020-07-08 23:39:34 -0300

    solve challenge4

--- challenge4/challenge4.js
+++ challenge4/challenge4.js
@@ -4,22 +4,80 @@ const user = {
     balance: 0
 }
 
-function createTransaction(transaction, value) {
+function createTransaction(transaction) {
 
-    if (transaction === "credit") {
-        user.balance = user.balance + value
-        user.transactions.push(value)
+    if (transaction.type === "credit") {
+        user.balance = user.balance + transaction.value
+        user.transactions.push(transaction)
     } else {
-        user.balance = user.balance - value
-        user.transactions.push("-" + value)
+        user.balance = user.balance - transaction.value
+        user.transactions.push(transaction)
     }
     console.log(user);
+}
+
+function getHigherTransactionByType(transactionType) {
+    let higherTransaction = []
+    let higherValue = 0
+
+    for (let transaction of user.transactions)
+        if (transaction.type == transactionType && transaction.value > higherValue) {
+            higherValue = transaction.value
+            higherTransaction = transaction
+        }
+    console.log(higherTransaction);
+}
+
+function getHigherTransaction() {
+    let higherTransaction = []
+    let higherValue = 0
+
+    for (let transaction of user.transactions)
+        if (transaction.value > higherValue) {
+            higherValue = transaction.value
+            higherTransaction = transaction
+        }
+    console.log(`Highest transaction is a ${higherTransaction.type} of R$ ${higherTransaction.value}`);
 
 }
 
-createTransaction("credit", 10)
-createTransaction("credit", 10)
-createTransaction("credit", 10)
-createTransaction("debit", 10)
-createTransaction("debit", 10)
-createTransaction("debit", 10)
+function getAverageTransactionValue() {
+    let sum = 0
+    for (let transaction of user.transactions) {
+        sum = sum + transaction.value
+    }
+    let average = sum / user.transactions.length
+    console.log(`The avarage is: R$ ${average.toFixed(2)}`);
+}
+
+function getTransactionsCount() {
+    let transactionCount = {
+        credit: 0,
+        debit: 0
+    }
+    for (let transaction of user.transactions) {
+        if (transaction.type == "credit") {
+            transactionCount.credit += 1
+        } else {
+            transactionCount.debit += 1
+        }
+    }
+    console.log(transactionCount);
+
+}
+
+createTransaction({ type: "credit", value: 100 })
+createTransaction({ type: "credit", value: 240 })
+createTransaction({ type: "credit", value: 24 })
+createTransaction({ type: "debit", value: 110 })
+createTransaction({ type: "debit", value: 100 })
+createTransaction({ type: "debit", value: 2120 })
+
+getHigherTransactionByType("credit")
+getHigherTransactionByType("debit")
+
+getHigherTransaction()
+
+getAverageTransactionValue()
+
+getTransactionsCount()
